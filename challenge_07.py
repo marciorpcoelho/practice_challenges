236962d0c3e913526004504b753b99300232931e
marciorpcoelho <marciorpcoelho@gmail.com>
2018-07-31 09:48:16 +0100

    - Completed challenge 07;

---
+++ challenge_07.py
@@ -0,0 +1,14 @@
+# Let’s say I give you a list saved in a variable: a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100].
+# Write one line of Python that takes this list a and makes a new list that has only the even elements of this list in it.
+
+
+def main():
+    a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+    even_list = [value for value in a if not (value % 2)]
+
+    print(even_list)
+
+
+if __name__ == '__main__':
+    main()
