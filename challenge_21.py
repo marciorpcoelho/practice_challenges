e3c98b2a9a1811244071cd7e4cece1d9af70ff69
marciorpcoelho <marciorpcoelho@gmail.com>
2018-07-31 17:52:05 +0100

    - Completed Challenge 21;

---
+++ challenge_21.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+
+# Take the code from the How To Decode A Website exercise and instead of printing the results to a screen, write the results to a txt file.
+# In your code, just make up a name for the file you are saving to.
+
+# Extras:
+# Ask the user to specify the name of the output file that will be saved.
+
+
+def main():
+    save_name = input('What is the name to save the file under? ')
+
+    url = 'http://www.vanityfair.com/society/2014/06/monica-lewinsky-humiliation-culture'
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html5lib')
+
+    contents = soup.find_all(class_="content-section")
+    with open(save_name + '.txt', 'w') as open_file:
+        for content in contents:
+            open_file.write(content.get_text())
+
+
+
+def challenge_19():
+   print()
+
+
+if __name__ == '__main__':
+    main()
